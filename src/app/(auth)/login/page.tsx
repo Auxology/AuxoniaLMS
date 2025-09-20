7f8ddc3184e2919c93bc7d6761224ee788094772
Akaki Jomidava <akaki.jomidavayt@gmail.com>
2025-09-20 15:42:54 +0400

    feat: add GitHub authentication

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,24 @@
+"use server";
+
+import LoginForm from "@/features/auth/components/forms/login-form";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function LoginPage() {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (session) {
+        return redirect("/");
+    }
+
+    return (
+        <section className="min-h-svh w-full flex flex-col items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-md">
+                <LoginForm />
+            </div>
+        </section>
+    )
+}
